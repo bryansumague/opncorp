e0a09259735617da09c3408fdc3ff667b9f6cc33
Bryan Sumague <bryan.sumague@leadingtech.com.vn>
2022-05-13 19:43:19 +0800

    Search bar on navigation change border color

--- src/common/shared/Navigation.tsx
+++ src/common/shared/Navigation.tsx
@@ -44,6 +44,7 @@ export default function Navbar({ isVisibility }: Props) {
 				margin: '0 auto',
 				boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.06)',
 				display: 'flex',
+				padding: '0.5rem',
 			}}
 		>
 			<ToolbarStyled>
@@ -54,75 +55,79 @@ export default function Navbar({ isVisibility }: Props) {
 						</i>
 					</Link>
 				</Box>
-				<Box
-					sx={{
-						width: '512px',
-						display: {
-							sx: 'none',
-							sm: 'none',
-							md: isVisibility ? 'flex' : 'none',
-						},
-					}}
-				>
-					<Grid container sx={{ position: 'relative' }}>
-						<TextField
-							fullWidth
-							sx={{
-								color: '#000000',
-								'& .MuiOutlinedInput-root': {
-									'& > fieldset': { borderColor: 'transparent' },
-								},
-							}}
-							placeholder='Search here...'
-							InputProps={{
-								style: {
-									height: '48px',
-									background: '#F7F7F7',
-									borderRadius: '2px',
-								},
-							}}
-						/>
-						<Box>
-							<Button
-								variant='contained'
-								color='secondary'
+				{isVisibility ? (
+					<Box
+						sx={{
+							width: '512px',
+							display: {
+								sx: 'none',
+								sm: 'none',
+								md: 'flex',
+							},
+						}}
+					>
+						<Grid container sx={{ position: 'relative' }}>
+							<TextField
+								fullWidth
 								sx={{
-									position: 'absolute',
-									right: 0,
-									bottom: 0,
-									top: 0,
-									height: '48px',
-									borderRadius: '0px 2px 2px 0px',
+									color: '#000000',
+									'& .MuiOutlinedInput-root': {
+										'& > fieldset': { borderColor: 'transparent' },
+									},
 								}}
-							>
-								<span style={{ marginTop: '10px' }}>
-									<i>
-										<svg
-											width='24'
-											height='24'
-											viewBox='0 0 24 24'
-											fill='none'
-											xmlns='http://www.w3.org/2000/svg'
-										>
-											<path
-												fill-rule='evenodd'
-												clip-rule='evenodd'
-												d='M11 4C7.13401 4 4 7.13401 4 11C4 14.866 7.13401 18 11 18C14.866 18 18 14.866 18 11C18 7.13401 14.866 4 11 4ZM2 11C2 6.02944 6.02944 2 11 2C15.9706 2 20 6.02944 20 11C20 15.9706 15.9706 20 11 20C6.02944 20 2 15.9706 2 11Z'
-												fill='white'
-											/>
-											<path
-												fill-rule='evenodd'
-												clip-rule='evenodd'
-												d='M15.9428 15.9429C16.3333 15.5524 16.9665 15.5524 17.357 15.9429L21.707 20.2929C22.0975 20.6834 22.0975 21.3166 21.707 21.7071C21.3165 22.0977 20.6833 22.0977 20.2928 21.7071L15.9428 17.3571C15.5523 16.9666 15.5523 16.3334 15.9428 15.9429Z'
-												fill='white'
-											/>
-										</svg>
-									</i>
-								</span>
-							</Button>
-						</Box>
-					</Grid>
-				</Box>
+								placeholder='Search here...'
+								InputProps={{
+									style: {
+										height: '48px',
+										background: '#F7F7F7',
+										borderRadius: '2px',
+									},
+								}}
+							/>
+							<Box>
+								<Button
+									variant='contained'
+									color='secondary'
+									sx={{
+										position: 'absolute',
+										right: 0,
+										bottom: 0,
+										top: 0,
+										height: '48px',
+										borderRadius: '0px 2px 2px 0px',
+									}}
+								>
+									<span style={{ marginTop: '10px' }}>
+										<i>
+											<svg
+												width='24'
+												height='24'
+												viewBox='0 0 24 24'
+												fill='none'
+												xmlns='http://www.w3.org/2000/svg'
+											>
+												<path
+													fill-rule='evenodd'
+													clip-rule='evenodd'
+													d='M11 4C7.13401 4 4 7.13401 4 11C4 14.866 7.13401 18 11 18C14.866 18 18 14.866 18 11C18 7.13401 14.866 4 11 4ZM2 11C2 6.02944 6.02944 2 11 2C15.9706 2 20 6.02944 20 11C20 15.9706 15.9706 20 11 20C6.02944 20 2 15.9706 2 11Z'
+													fill='white'
+												/>
+												<path
+													fill-rule='evenodd'
+													clip-rule='evenodd'
+													d='M15.9428 15.9429C16.3333 15.5524 16.9665 15.5524 17.357 15.9429L21.707 20.2929C22.0975 20.6834 22.0975 21.3166 21.707 21.7071C21.3165 22.0977 20.6833 22.0977 20.2928 21.7071L15.9428 17.3571C15.5523 16.9666 15.5523 16.3334 15.9428 15.9429Z'
+													fill='white'
+												/>
+											</svg>
+										</i>
+									</span>
+								</Button>
+							</Box>
+						</Grid>
+					</Box>
+				) : (
+					''
+				)}
 				<Box display='flex' sx={{ gap: { xs: '12px', sm: '12px' } }}>
 					<Button
 						variant='outlined'
